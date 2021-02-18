f5f38c3e5b202976d280bcc3f1af5020f8012f45
Wojciech Iskra <wojciech.iskra@schibsted.com>
2021-02-18 08:58:39 +0100

    Testing eslint step in CI

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = ({ logger = console, level = 'error' } = {}) => {
 
             // if there are a `statusCode` and an `error` field
             // this is a valid http error object
-            if (typeof logger[level] === 'function') {
+            if (typeof logger[level] === 'function')
+
+            {
                 logger[level](
                     {
                         error: R.pick(['name', 'message', 'stack', 'details', 'status', 'statusCode', 'expose'], error),
