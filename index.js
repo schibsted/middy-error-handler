57f804b750edbea9d995da49efe19cea58468709
Wojciech Iskra <wojciech.iskra@schibsted.com>
2021-07-27 09:26:06 +0200

    feat: middy v2 compatible version
    also updated rest of the deps

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const R = require('ramda');
 
-module.exports = ({ logger = console, level = 'error' } = {}) => {
-    return {
-        onError: async (handler) => {
-            const { error } = handler;
+module.exports = ({ logger = console, level = 'error' } = {}) => ({
+    onError: async (handler) => {
+        const { error } = handler;
 
-            // if there are a `statusCode` and an `error` field
-            // this is a valid http error object
-            if (typeof logger[level] === 'function') {
-                logger[level](
-                    {
-                        error: R.pick(['name', 'message', 'stack', 'details', 'status', 'statusCode', 'expose'], error),
-                    },
-                    `${R.prop('name', error)}: ${R.prop('message', error)}`
-                );
-            }
+        // if there are a `statusCode` and an `error` field
+        // this is a valid http error object
+        if (typeof logger[level] === 'function') {
+            logger[level](
+                {
+                    error: R.pick(['name', 'message', 'stack', 'details', 'status', 'statusCode', 'expose'], error),
+                },
+                `${R.prop('name', error)}: ${R.prop('message', error)}`
+            );
+        }
 
-            // eslint-disable-next-line no-param-reassign
-            handler.response = {
-                ...R.propOr({}, 'response', handler),
-                statusCode: R.propOr(500, 'statusCode', error),
-                body: JSON.stringify(
-                    R.filter(Boolean, {
-                        statusCode: R.prop('statusCode', error),
-                        message: R.prop('message', error),
-                        details: R.prop('details', error),
-                        stack: !['test', 'production'].includes(process.env.NODE_ENV) && R.prop('stack', error),
-                    })
-                ),
-            };
+        // eslint-disable-next-line no-param-reassign
+        handler.response = {
+            ...R.propOr({}, 'response', handler),
+            statusCode: R.propOr(500, 'statusCode', error),
+            body: JSON.stringify(
+                R.filter(Boolean, {
+                    statusCode: R.prop('statusCode', error),
+                    message: R.prop('message', error),
+                    details: R.prop('details', error),
+                    stack: !['test', 'production'].includes(process.env.NODE_ENV) && R.prop('stack', error),
+                })
+            ),
+        };
 
-            return handler;
-        },
-    };
-};
+        return handler.response;
+    },
+});
